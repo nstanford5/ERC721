4ddf5a0a32363f725c6b15de7d2b0de8446e72da
Nick Stanford <nick@reach.sh>
2023-01-04 19:00:32 -0700

    not done

--- index.mjs
+++ index.mjs
@@ -35,6 +35,13 @@ const assertEq = (a, b, context = "assertEq") => {
   stdlib.assert(false, `${context}: ${a} == ${b}`);
 }
 
+const lock = () => {
+  const lockObj = {};
+  lockObj.reset = () => lockObj.wait = new Promise(r => { lockObj.unlock = r; });
+  lockObj.reset();
+  return lockObj;
+;}
+
 const startMeUp = async (ctc, meta) => {
   const flag = "startup success throw flag";
   try {
@@ -65,8 +72,51 @@ await startMeUp(ctc0, meta);// contract deployed
 const ctcInfo = await ctc0.getInfo();
 const ctc = (acc) => acc.contract(backend, ctcInfo);
 
+/**
+ * Locking mechanism.
+ * 
+ * Do I need this? Or can I make it work
+ * 
+ * Finish locking mechanism
+ */
+const evLocks = {};
+const assertEvent = {};
+for (const ev of ["Transfer", "Approval", "ApprovalForAll"]){
+  // Make lock, to wait for an event to occur
+  const l = lock();
+  ctc.on(ev, (...args) => l.unlock(args));
+  evLocks[ev] = l;
 
-//await assertFail(ctc0.v.balanceOf(zeroAddr));
+  // Event occurence assertion helper
+  assertEvent[ev] = async (...expectedArgs) => {
+    const args = await l.wait;
+    l.reset();
+    expectedArgs.forEach((expectedArg, i) => assertEq(args[i], expectedArg, `${ev} field ${i}`));
+  }
+}// end of for loop
+
+// other helpers
+const assertOwners = async (...owners) => {
+  assertEq(await ctc.ownerOf(tok1), owners[0], "ownerOf(1)");
+
+  const countAddr = addr => owners.reduce((n, a) => n + (addr === a ? 1 : 0), 0);
+  assertEq(await ctc.balanceOf(addr1), countAddr(addr1), "balanceOf(addr1)");
+}
+// networkAccount: https://docs.reach.sh/frontend/#js_networkAccount
+const [_, ctc1, ctc2, ctc3] = accs.map(a => ctc.connect(a.networkAccount));
+const mkTransfer = transferFn => async (from, to, tok, ctcOverride) => {
+  const fromCtc = ctcOverride ?? (from === addr1 ? ctc1 : (from === addr2 ? ctc2 : ctc3));
+  await waitTxn(fromCtc[transferFn](from, to, tok, gasLimit));
+  await assertEvent.Approval(from, zeroAddr, tok);
+  await assertEvent.Transfer(from, to, tok);
+};
+const transferFrom = mkTransfer("transferFrom");
+const safeTransferFrom = mkTransfer("safeTransferFrom(address,address,uint256)");
+
+
+
+
+await assertFail(ctc.balanceOf(zeroAddr));
 
 
 console.log('Exiting...');
